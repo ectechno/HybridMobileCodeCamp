94d9d7cf952bb50546a6310432f9176028e9e14e
nimeshc64 <nimeshc64@gmail.com>
2015-08-05 03:14:01 -0700

    create new app with REST API and new desing

--- Nimesh Chathuranga/CodeCamp/www/js/app.js
+++ Nimesh Chathuranga/CodeCamp/www/js/app.js
@@ -237,29 +237,30 @@ app.controller('UserController', function($scope, $state, $ionicPopup) {
     }
 })
 //------------------------------camera controller--------------------------------------
-app.controller("CameraController", function($scope, $cordovaCamera) {
-
+app.controller('CameraCtrl', function($ionicPlatform, $rootScope, $scope, $cordovaCamera) {
+    $ionicPlatform.ready(function() {
+        var options = {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            allowEdit: true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 100,
+            targetHeight: 100,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false
+        };
+ 
         $scope.takePicture = function() {
-            var options = {
-                quality : 75,
-                destinationType : Camera.DestinationType.DATA_URL,
-                sourceType : Camera.PictureSourceType.CAMERA,
-                allowEdit : true,
-                encodingType: Camera.EncodingType.JPEG,
-                targetWidth: 300,
-                targetHeight: 300,
-                popoverOptions: CameraPopoverOptions,
-                saveToPhotoAlbum: false
-            };
-
             $cordovaCamera.getPicture(options).then(function(imageData) {
-                $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                $scope.imgSrc = "data:image/jpeg;base64," + imageData;
             }, function(err) {
-                // An error occured. Show a message to the user
+                console.log(err);
             });
         }
-
+ 
     });
+})
 //----------------------------------------------------------------------------------------------------------
 //.run(function($ionicPlatform) {
 //  $ionicPlatform.ready(function() {
